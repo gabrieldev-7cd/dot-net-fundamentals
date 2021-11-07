672cacf887a9fe4c7808132f3a825ba7280e5e3c
gabrieldev7cd <gabriel.alexandre.dev@gmail.com>
2021-11-07 16:19:19 -0300

    Init Project

---
+++ DotNetFundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
