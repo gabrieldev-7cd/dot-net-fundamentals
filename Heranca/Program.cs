8c57d2f7072693fb80fb3d2688caff329d610d7b
gabrieldev7cd <gabriel.alexandre.dev@gmail.com>
2021-11-08 08:44:58 -0300

    Herança

---
+++ Heranca/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Heranca
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        private double _Saldo { get; set; }
+       
+        // A assinatura virtual permite a alteração do método (Polimorfismo).
+        protected virtual void GetSaldo()
+        {
+            Console.WriteLine($"\n\nO saldo da conta de nº{Numero} é {_Saldo}");
+        }
+
+        public void SetSaldo(double saldo)
+        {
+            if(saldo >= 0)
+            {
+                _Saldo += saldo;
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido!");
+            }
+        }
+    }
+
+    class ContaPoupanca : Conta
+    {
+        public int JurosMensais { get; set; }
+
+        // Override indica a sobrescrição do método da classe pai.
+        protected override void GetSaldo()
+        {
+            Console.WriteLine("Este é o método alteradaço e o valor de saldo agora é R$ 43462847234424,47");
+        }
+
+        public ContaPoupanca(int numero, double saldo)
+        {
+            Numero = numero;
+            SetSaldo(saldo);
+        }
+
+        static void Main(string[] args)
+        {
+            ContaPoupanca contaPoupanca = new ContaPoupanca(42, 19.99);
+            contaPoupanca.GetSaldo();
+        }
+    }
+}
